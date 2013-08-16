ec304d071b6615a06995f1dafa292072b5a2d7f0
Sean <sean.brookes@elasticpath.com>
2013-08-16 15:26:14 -0700

    stupid changes

---
+++ public/ep.mediator.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright © 2013 Elastic Path Software Inc. All rights reserved.
+
+ * User: sbrookes
+ * Date: 15/08/13
+ * Time: 3:49 PM
+ *
+ */
+define(['eventbus'],function(EventBus){
+
+  var mediatorObj = {
+    'mediator.loadLogoComponentRequest':function(){
+      require(['appheader'],function(mod){
+        EventBus.trigger('appheader.loadLogoComponent',reqEventData);
+      });
+    }
+  };
+
+
+  function fire(){
+
+    var args = arguments;
+    if (args.length > 0){
+      var reqEventName  = args[0];
+      var reqEventData;
+      if (args.length > 1){
+        reqEventData = args[1];
+      }
+
+      mediatorObj[reqEventName](reqEventData);
+
+
+
+
+
+
+
+    }
+
+
+
+  }
+
+
+  return {
+    fire:fire
+  };
+});
