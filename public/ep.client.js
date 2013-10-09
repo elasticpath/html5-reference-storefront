42acf9afda02b83f525cacdf44211f5b8a924cd3
Sean <sean.brookes@elasticpath.com>
2013-10-08 22:57:50 -0700

    reset app init code back into ep-client as it was causing some loading instabilty

--- public/ep.client.js
+++ public/ep.client.js
@@ -283,7 +283,41 @@ define(['jquery', 'underscore', 'backbone', 'marionette', 'mediator', 'eventbus'
      * end user prefs
      * */
 
+    // bootstrap initialization complete (main.js)
+    // time to start up the application
+    EventBus.on('app.bootstrapInitSuccess', function () {
+      // when ready start the router
+      ep.app.addInitializer(function (options) {
+        // do useful stuff here
+        ep.router = new Router.AppRouter();
+      });
+      // wait until the application and DOM are spun up
+      // then start the history manager
+      ep.app.on("initialize:after", function () {
+        if (Backbone.history) {
+          //Backbone.history.start({ pushState: true });
+          Backbone.history.start();
+        }
+      });
 
+
+
+
+
+      EventBus.trigger('ep.startAppRequest');
+
+
+    });
+
+    EventBus.on('ep.startAppRequest', function () {
+      // turn the key and give 'er some gass
+      try {
+        ep.app.start();
+      }
+      catch (e) {
+
+      }
+    });
 
 
 
