00472662718e15fb0cdd4741c585050c8d1b6a6a
ama <angela.ma@elasticpath.com>
2013-10-22 13:08:16 -0700

    [CU-144] fixing content injected into appHeader problem (not tested)

--- public/modules/base/home/base.home.views.js
+++ public/modules/base/home/base.home.views.js
@@ -9,30 +9,35 @@
 define(['backbone','eventbus'],
   function(Backbone,EventBus){
 
-    var DefaultHomeView = Backbone.Marionette.ItemView.extend({
-      template:'#HomeModuleDefaultTemplate',
-      className:'home-container',
-      onRender:function(){
-//        (function(){
-//          var script = document.createElement('script');
-//
-//          script.async = true;
-//          script.src = 'http://10.10.121.182:3008/embed/widget.js';
-//
-//          var entry = document.getElementsByTagName('script')[0];
-//          entry.parentNode.insertBefore(script, entry);
-//
-//        }());
-      }
-//      onShow:function(){
-//        $('[data-region="scriptInclude"]').html('<script src="http://10.10.121.182:3008/embed/widget.js"></script>');
-//      }
-    });
     var DefaultHomeLayout = Backbone.Marionette.Layout.extend({
-      template:'#HomeModuleDefaultLayoutTemplate',
-      tagName:'div',
-      className:'container'
+      template:'#DefaultHomeLayoutTemplate',
+      regions: {
+        homeContentRegion: '[data-region="homeMainContentRegion"]'
+      }
     });
+
+    var DefaultHomeView = Backbone.Marionette.ItemView.extend({
+      template:'#DefaultHomeViewTemplate',
+      className:'home-container'
+/*
+      onRender:function(){
+        (function(){
+          var script = document.createElement('script');
+
+          script.async = true;
+          script.src = 'http://10.10.121.182:3008/embed/widget.js';
+
+          var entry = document.getElementsByTagName('script')[0];
+          entry.parentNode.insertBefore(script, entry);
+
+        }());
+      }
+      onShow:function(){
+        $('[data-region="scriptInclude"]').html('<script src="http://10.10.121.182:3008/embed/widget.js"></script>');
+      }
+*/
+    });
+
     return{
       DefaultHomeView:DefaultHomeView,
       DefaultHomeLayout:DefaultHomeLayout
