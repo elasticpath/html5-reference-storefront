85e36023e6b518023761514fc4f18cb177512505
ama <angela.ma@elasticpath.com>
2013-11-19 14:34:12 -0800

    initial try to create documented example of new module

--- public/modules/_templates/tmpl.controller.js
+++ public/modules/_templates/tmpl.controller.js
@@ -1,21 +1,31 @@
 /**
  * Copyright Elastic Path Software 2013.
-
- * User: sbrookes
- * Date: 04/04/13
- * Time: 9:16 AM
  *
- * 
+ * Example controller template to creating a new module on Storefront.
  */
-define(['app', 'eventbus', 'modules/_template/tmpl.models', 'modules/_template/tmpl.views', 'text!modules/_template/tmpl.templates.html'],
-  function(App, EventBus, Model, View, template){
+define(function (require) {
+    var ep = require('ep');                 // import global app functions and variables
+    var EventBus = require('eventbus');     // import event-bus communicating within module
+    var Mediator = require('mediator');     // import global event-bus mediating communication between modules
 
+    var pace = require('pace');             // import activity indicator function
+
+    // import model, view and template
+    var Model = require('modules/_template/tmpl.models');
+    var View = require('modules/_template/tmpl.views');
+    var template = require('text!modules/base/_template/base.tmpl.templates.html');
+
+    // Inject the address template into TemplateContainer for the views to reference
     $('#TemplateContainer').append(template);
 
+    // Creates namespace to template to reference model and viewHelpers
     _.templateSettings.variable = 'E';
 
-    var init = function(){
-
+    /**
+     * Renders the DefaultLayout of template module, and fetch model from backend;
+     * upon model fetch success, renders the views in destinated regions.
+     */
+    var defaultView = function(){
 
 
     };
@@ -23,8 +33,7 @@ define(['app', 'eventbus', 'modules/_template/tmpl.models', 'modules/_template/t
 
 
     return {
-      init:init
-
+      DefaultView:defaultView
     };
   }
 );
--- public/modules/_templates/tmpl.models.js
+++ public/modules/_templates/tmpl.models.js
@@ -9,13 +9,20 @@
 define(['eventbus', 'backbone'],
   function(EventBus, Backbone){
 
+    var tmplModel = Backbone.Model.extend({
+      // url to READ data from cortex, can include zoom, take example below
+      // ep.app.config.cortexApi.path   prefix contex path to request to Cortex (e.g. integrator, cortex), configured in ep.config.json,
+      // ep.app.config.cortexApi.scope  store scope (e.g. mobee, telcooperative), configured in ep.config.json,
+      url: ep.app.config.cortexApi.path + '/profiles/' + ep.app.config.cortexApi.scope + '/default?zoom=purchases:element',
 
-
+      parse: function (response) {
+        // parsing the raw JSON data from cortex server using jsonPath
+      }
+    });
 
 
     return {
-
-
+      TmplModel: tmplModel
     };
   }
 );
