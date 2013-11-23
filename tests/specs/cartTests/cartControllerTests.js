3ff463992f45e3d2d2eb96e09f82d26e99d66898
Jamie Howells <jamie.howells@elasticpath.com>
2013-11-22 16:23:18 -0800

    [CU-162] unit tests for cart controller.

--- tests/specs/cartTests/cartControllerTests.js
+++ tests/specs/cartTests/cartControllerTests.js
@@ -6,6 +6,7 @@
 define(function (require) {
   var EventBus = require('eventbus'),
     Backbone = require('backbone'),
+    Marionette = require('marionette'),
     ep = require('ep'),
     EventTestFactory = require('EventTestFactory'),
     EventTestHelpers = require('EventTestHelpers');
@@ -254,9 +255,69 @@ define(function (require) {
 
 
 
+
+    // Event Listener: cart.checkoutBtnClicked
+    describe('cart.checkoutBtnClicked event works', function () {
+
+      describe('routed to checkout if user is logged in', function() {
+        var unboundEventKey = 'layout.loadRegionContentRequest';
+        var actionLink = 'ActionLinkTrue';
+
+        before(function () {
+          sinon.spy(EventBus, 'trigger');
+          sinon.stub(ep.app, 'isUserLoggedIn', function() {
+            return true;
+          });
+          ep.router = new Marionette.AppRouter();
+          sinon.stub(ep.router, 'navigate');
+
+          EventTestHelpers.unbind(unboundEventKey);
+          EventBus.trigger('cart.checkoutBtnClicked', actionLink);
+        });
+
+        after(function () {
+          EventTestHelpers.reset();
+          ep.app.isUserLoggedIn.restore();
+          EventBus.trigger.restore();
+          ep.router.navigate.restore();
+        });
+
+        it('fires a request for the checkout view', sinon.test(function () {
+          var regionObj = { region: 'appMainRegion', module: 'cart', view: 'CheckoutView' };
+          expect(EventBus.trigger).to.be.calledWithExactly('layout.loadRegionContentRequest', regionObj);
+        }));
+      });
+
+      describe('log in modal loaded if user is not logged in', function() {
+        var unboundEventKey = 'layout.loadRegionContentRequest';
+        var actionLink = 'ActionLinkTrue';
+
+        before(function () {
+          sinon.spy(EventBus, 'trigger');
+          sinon.stub(ep.app, 'isUserLoggedIn', function() {
+            return false;
+          });
+
+          EventTestHelpers.unbind(unboundEventKey);
+          EventBus.trigger('cart.checkoutBtnClicked', actionLink);
+        });
+
+        after(function () {
+          EventTestHelpers.reset();
+          ep.app.isUserLoggedIn.restore();
+          EventBus.trigger.restore();
+        });
+
+        it('fires a request for the login form', sinon.test(function () {
+          var regionObj = { region: 'appModalRegion', module: 'auth', view: 'LoginFormView' };
+          expect(EventBus.trigger).to.be.calledWithExactly('layout.loadRegionContentRequest', regionObj);
+        }));
+      });
+    });
+
     // Event Listener: cart.submitOrderBtnClicked
     describe("cart.submitOrderBtnClicked event works", function () {
-      var unboundEventKey = 'cart.checkoutRequest';
+      var unboundEventKey = 'cart.submitOrderRequest';
       var actionLink = 'ActionLinkTrue';
 
       before(function () {
@@ -275,11 +336,49 @@ define(function (require) {
       });
 
       it("fires cart.submitOrderRequest", sinon.test(function () {
-        expect(EventBus.trigger).to.be.calledWithExactly('cart.submitOrderRequest', actionLink);
+        expect(EventBus.trigger).to.be.calledWithExactly(unboundEventKey, actionLink);
       }));
       it('called View.setCheckoutButtonProcessing', sinon.test(function () {
         expect(View.setCheckoutButtonProcessing).to.be.called;
       }));
+    });
+
+    // Event Listener: cart.cancelOrderBtnClicked
+    describe('Responds to event: cart.cancelOrderBtnClicked',
+      EventTestFactory.simpleEventTriggersEventTest('cart.reloadCartViewRequest', 'cart.cancelOrderBtnClicked'));
+
+    // Checkout View
+    describe("CheckoutView", function () {
+      var cartTemplate = require('text!modules/base/cart/base.cart.templates.html');
+
+      before(function () {
+        sinon.stub(Backbone, 'sync');
+
+        $("#Fixtures").append(cartTemplate);
+        this.viewLayout = new cartController.CheckoutView();
+        this.viewLayout.render();
+      });
+
+      after(function () {
+        $("#Fixtures").empty();
+        Backbone.sync.restore();
+      });
+
+      it('CheckoutView should exist', function () {
+        expect(this.viewLayout).to.exist;
+      });
+      it('should be an instance of Marionette Layout object', function () {
+        expect(this.viewLayout).to.be.an.instanceOf(Marionette.Layout);
+      });
+      it('render() should return the view object', function () {
+        expect(this.viewLayout.render()).to.be.equal(this.viewLayout);
+      });
+      it('view\'s DOM is rendered with 1 child (view content rendered)', function () {
+        expect(this.viewLayout.el.childElementCount).to.be.equal(1);
+      });
+      it('Model should have fetched info from server once', function () {
+        expect(Backbone.sync).to.be.calledOnce;
+      });
     });
 
   });
