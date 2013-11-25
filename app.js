2a9f42470a3e1d96b40108c36e3d14ad45a05f25
Ivan Jensen <ivan.jensen@elasticpath.com>
2013-11-25 11:12:01 -0800

    [CU-151] Intitial cut at providing a simple extension model.  Simply add the files you wish to override in an 'ext/modules/base' folder at the same level as the ui-storefront folder.  This was done by simply adding a second directory to the express configuration where a different set of files can be loaded from.  Currently you can only override whole files, no capability to add new modules exists at the moment due to the need to change configurations.

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/', express.static(path.join(__dirname, '../ext')));
+  app.use('/', express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
