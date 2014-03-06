dafe0da41870c9c9445dcd980b27a1521ae7258f
unknown <jhowells@EP-WW-0425.elasticpath.net>
2014-03-06 11:43:52 -0800

    Added Apache license notices to banner comments.

--- public/modules/base/profile/base.profile.models.js
+++ public/modules/base/profile/base.profile.models.js
@@ -1,6 +1,18 @@
 /**
  * Copyright © 2014 Elastic Path Software Inc. All rights reserved.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * Default Profile Models
  * The HTML5 Reference Storefront's MVC Model manages zoom queries the model uses to ask cortex server.
  * It also parses the raw data returned from server, so necessary data are surfaced, and renamed into desired format.
