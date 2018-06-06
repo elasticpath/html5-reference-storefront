1e4ae205d2bce43dbbe581dedaf8dce0c1d978bb
Shaun Maharaj <Shaun.Maharaj@elasticpath.com>
2018-06-05 23:41:31 -0400

    - Clean app.js

--- app.js
+++ app.js
@@ -52,13 +52,6 @@ app.configure('development', function () {
   app.use(express.errorHandler());
 });
 
-app.use(function (req, res, next) {
-  if ((req.get('X-Forwarded-Proto') !== 'https')) {
-    res.redirect('https://' + req.get('Host') + req.url);
-  } else
-    next();
-});
-
 /*
  *
  * ROUTES
